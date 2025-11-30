ca8b420e5a0d0a9767b119b616055c5880e3b2ed
JherosjaY <151810228+JherosjaY@users.noreply.github.com>
2025-11-30 15:11:19 +0800

    Fix: Change repositoriesMode to PREFER_SETTINGS in settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,3 @@ plugins {
     alias(libs.plugins.android.application) apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
